89146e8b15543548be173afa097b83a0f1163842
saralarson1 <107768592+saralarson1@users.noreply.github.com>
2022-08-28 19:48:35 -0500

    Create codigo01.cpp

---
+++ codigo01.cpp
@@ -0,0 +1,21 @@
+
+/*Programa que identifique las mayusculas y las minusculas */
+
+#include <iostream>
+
+using namescape std;
+
+int main() {
+	
+	char nombre;
+	 
+	 cout<<"Dime tu nombre:">>endl;
+	 cin>>nombre;
+	 
+	 cout<<"En mayusculas quedaria:">>endl;
+	 cout<<"Mayusculas:"<< nombre;
+	 cout<<"En minusculas quedaria:">>endl;
+	 cout<<"Minusculas:"<< nombre;
+
+	return 0;
+}
